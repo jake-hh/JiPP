087f9334de7c8c6f2110328c15cb0fc11d485cb3
jake-hh <jakob.holzer.m@gmail.com>
2025-12-08 19:16:58 +0100

    Lab8 zad7

---
+++ lab8/zad8_7.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable: 4996)
+
+extern void error(int, char *);
+
+extern double **DajMac_1(int n, int m);
+extern void ZwrocMac_1(double **ma, int n);
+extern void CzytMac(FILE *fd, double **ma, int n, int m);
+
+void argumenty(int, char **);
+
+
+int main(int argc, char *argv[]) {
+	FILE *fd;
+	double **ma, a, b, sum = 0;
+	int n;
+
+	// -- Open file & read data --
+	argumenty(argc, argv);
+	if (!(fd = fopen(argv[1], "r"))) error(2, "dane");
+
+	fscanf(fd, "%d %lf %lf", &n, &a, &b);
+	if (n < 1) error(1, "n < 1");
+
+	// -- Dynamically allocate memory --
+	ma = DajMac_1(n, n);
+
+	// -- Read matrix --
+	CzytMac(fd, ma, n, n);
+
+	// -- Calculate --
+	for (int r = 1; r < n; r++)
+		for (int c = 0; c < r; c++)
+			if (ma[r][c] >= a && ma[r][c] <= b)
+				sum += ma[r][c];
+
+	printf("a: %g\n", a);
+	printf("b: %g\n", b);
+	printf("Suma elementów pod przekątną z przedziału (a, b): %g\n", sum);
+
+	// -- Free --
+	ZwrocMac_1(ma, n);
+	return 0;
+}
+
+
+void argumenty(int argc, char *argv[]) {
+	int len;
+	char *usage;
+	if (argc != 2) {
+		len = strlen(argv[0]) + 19;
+
+		if (!(usage = (char*)malloc((unsigned)len * sizeof(char))))
+			error(3, "tablica usage");
+
+		strcpy(usage, argv[0]);
+		strcat(usage, " file_in");
+		error(4, usage);
+	}
+}
