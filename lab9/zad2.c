472528394e79749cb1315a0593c7268ae12f1013
jake-hh <jakob.holzer.m@gmail.com>
2025-12-09 09:32:03 +0100

    Lab9 zad3

---
+++ lab9/zad2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+// double funkcja(double x, double (*f)(double)) {
+double funkcja(double x, double f(double)) {
+	return x * x + (*f)(x);
+}
+
+int main() {
+	double x = 0.0;
+	printf("dla x = %g, f(x) = %g\n", funkcja(x, sin));
+	printf("dla x = %g, f(x) = %g\n", funkcja(x, cos));
+	return 0;
+}
